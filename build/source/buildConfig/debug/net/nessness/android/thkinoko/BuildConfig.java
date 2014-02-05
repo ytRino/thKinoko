3f4f9b21257d3661e2a240a60155197cff4adf70
ytRino <ytrino@gmail.com>
2014-02-05 22:30:19 +0900

    first commit

---
+++ build/source/buildConfig/debug/net/nessness/android/thkinoko/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package net.nessness.android.thkinoko;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "net.nessness.android.thkinoko";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 301;
+}
